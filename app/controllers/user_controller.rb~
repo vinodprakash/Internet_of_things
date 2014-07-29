264a1410b380b7ff527d6a0b2500bcb8ac98b1f0
vinodprakash <psvinodprakash@gmail.com>
2014-07-30 00:47:59 +0530

    Performing Background Jobs

--- app/controllers/user_controller.rb~
+++ app/controllers/user_controller.rb~
@@ -1,7 +1,29 @@
 class UserController < ApplicationController
   def index
     @current = User.all
-      PrintWorker.perform_async(params[:print])
-  
+    current = User.all
+    #PrintWorker.perform_async(params[:print])
+    #PrintWorker.perform_async('vinod')
+    x = [] 
+    y = []
+    z = []
+    current.each do |job|
+    		x << job["email"]
+    		y << job["sign_in_count"]
+                z << job["username"]
+		  end
+      mobile = Mobile.all
+   
+    name = []
+    model = []
+    mobile.each do |job|
+    		
+    		name << job["name"]
+                model << job["model"]
+		  end
+   
+    PrintWorker.perform_async(x,y,z,name,model)
+ 
+        
   end
 end
--- app/workers/print_worker.rb
+++ app/workers/print_worker.rb
@@ -1,7 +1,64 @@
 class PrintWorker
   include Sidekiq::Worker
  
-  def perform(str)
-    puts str
+  def perform(email,signd,uname,name,model)
+   # puts str 
+    puts "List Of Users and their Details"
+    puts " "
+ 	i=0
+     email.each do
+        print i
+        print " "
+	puts email[i]
+        i=i+1
+	end
+	i=0
+   puts " "
+   puts "List Of Username"
+    puts " "
+     uname.each do
+        print i
+        print " "
+	puts uname[i]
+        i=i+1
+	end
+    puts " "
+   puts "List Of Users Signed In count"
+    puts " "
+     i=0
+     signd.each do
+        print i
+        print " "
+	puts signd[i]
+        i=i+1
+	end
+
+ 
+puts " "
+   puts "List Of MObile Name"
+    puts " "
+     i=0
+     name.each do
+        print i
+        print " "
+	puts name[i]
+        i=i+1
+	end
+
+  puts " "
+   puts "List Of Mobile Model"
+    puts " "
+     i=0
+     model.each do
+        print i
+        print " "
+	puts model[i]
+        i=i+1
+	end
+
+   print "hello"
+   print "sidekiq"
   end
+
+ 
 end
