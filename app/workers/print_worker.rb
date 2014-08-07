8d91d7d9e47da3a47918404f0291a85daa9f110f
vinodprakash <psvinodprakash@gmail.com>
2014-08-07 15:02:08 +0530

    Storing details in mongodb

--- app/workers/print_worker.rb
+++ app/workers/print_worker.rb
@@ -24,7 +24,16 @@ class PrintWorker
    data = resp.body
    result = JSON.parse(data)
    puts "Recieved Json-details : #{result}"
+   id=result["id"]
+   name = result["name"]
+   model = result["model"]
+   createdat =result["created_at"]
+   updatedat =result["updated_at"]
+   userid = result["user_id"]
    
+   db = Mongo::Connection.new.db('iot-sidekiq')
+     coll = db.collection("mobile")
+                coll.insert({:Mobile_id =>id, :Name =>name,:Model =>model, :Created_at =>createdat,:Last_Update =>updatedat, :Created_by_User =>userid})
 
   end
  end
